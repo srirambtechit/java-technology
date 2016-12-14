337aa3a09503ff590f8b88b71ba67ec19f85cbfd
srirambtechit <srirambtecit@gmail.com>
2016-12-14 21:28:47 +0530

    update LambdaExpression.java

---
+++ project-lambda-java8/src/com/msrm/lambda/usecases/LambdaExpressions.java
@@ -0,0 +1,37 @@
+package com.msrm.lambda.usecases;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class LambdaExpressions {
+
+	interface Operation<T> {
+		public T perform(T t1, T t2);
+	}
+
+	public static void main(String[] args) {
+		// Lambda expression - () -> { } 
+		Operation<Integer> add = (Integer a, Integer b) -> { return a + b; };
+		System.out.println("Add : " + add.perform(3, 4));
+		
+		// Lambda expression - Automatically type inferred
+		Operation<Integer> sub = (a, b) -> { return a - b; };
+		System.out.println("Sub : " + sub.perform(3, 4));
+
+		// Lambda expression - if one statement no need of { }
+		Operation<Integer> mul = (a, b) -> a * b;
+		System.out.println("Mul : " + mul.perform(3, 4));
+
+		// Lambda expression - Thread spawing
+		Runnable r = () -> System.out.println("Hello");
+		new Thread(r).start();
+
+		// Method references
+		Comparable<String> cmpbl = String::length;
+
+		Comparator<String> cmptr = (a, b) -> a.toUpperCase().compareTo(b.toLowerCase());
+		
+	}
+
+}
